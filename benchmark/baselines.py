07dd373004217ad3b9541bc93aceb6c31e77772b
zz242msu <121355649+zz242msu@users.noreply.github.com>
2023-05-26 10:00:35 -0400

    Update baselines.py edited celf and celfpp

--- benchmark/baselines.py
+++ benchmark/baselines.py
@@ -6,7 +6,7 @@ import ndlib
 import ndlib.models.epidemics as ep
 import ndlib.models.ModelConfig as mc
 import statistics as s
-import heapdict as hd
+# import heapdict as hd
 import random
 import heapq
 
@@ -254,70 +254,310 @@ def greedyLT(g, config, budget):
 # baseline 8 sketch base
 
 # baseline 9.1 CELF
+# Function definition with inputs of a graph 'g', a configuration 'config', and a budget 'budget'
 def celf(g, config, budget):
 
+    # Initialize 'selected' as an empty list. This list will eventually hold the selected nodes
     selected = []
+    
+    # Get a list of all nodes in the graph 'g'
     candidates = list(g.nodes())
 
+    # Initialize 'gains' as an empty list. This list will hold the gain of each node
     gains = []
+
+    # For each node in the list of candidates
     for node in candidates:
+        # Create a new list 'seed' that includes all currently selected nodes plus the current node
         seed = selected + [node]
+        
+        # Run the IC function on the graph with the 'seed' list and get the result
         result = IC(g, config, seed)
+        
+        # Calculate the average (mean) of the result and assign it to 'gain'
         gain = s.mean(result)
-        # heapq min-heap
+        
+        # Add a tuple of negative 'gain' and 'node' to the 'gains' list
         heapq.heappush(gains, (-gain, node))
 
+    # Repeat the next steps 'budget' times
     for i in range(budget):
+        # Keep looping until a condition is met
         while True:
+            # Get the tuple with the highest gain (which will be the first in the list due to the sorting in the heap)
             gain, node = heapq.heappop(gains)
+            
+            # Again, create a new list 'seed' that includes all currently selected nodes plus the current node
             seed = selected + [node]
+            
+            # Run the IC function on the graph with the 'seed' list and get the result
             result = IC(g, config, seed)
+            
+            # Calculate the average (mean) of the result and assign it to 'new_gain'
             new_gain = s.mean(result)
+            
+            # If 'new_gain' equals the negation of 'gain'
             if new_gain == -gain:
+                # Break the loop
                 break
             else:
+                # Otherwise, add the tuple of negative 'new_gain' and 'node' back to the 'gains' list
                 heapq.heappush(gains, (-new_gain, node))
 
+        # Append the current node to the 'selected' list
         selected.append(node)
+        
+        # Remove the current node from the 'candidates' list
         candidates.remove(node)
+        print(sorted(selected))
 
-    return selected
+    # Return the list of selected nodes
+    return (sorted(selected))
+
+def celf2(g, config, budget):   
+    """
+    Inputs: g:     Network graph
+            config: Configuration object for the IC model
+            budget:     Size of seed set
+    Return: A seed set of nodes as an approximate solution to the IM problem
+    """
+      
+    # --------------------
+    # Find the first node with greedy algorithm
+    # --------------------
+    
+    # Compute marginal gain for each node
+    candidates = list(g.nodes())
+    #, start_time = list(g.nodes()), time.time()
+    # step 1, call our IC function, get the result of list
+    # step 2, calculate the margin gain 
+    marg_gain = [s.mean(IC(g, config, [node])) for node in candidates]
+
+
+
+    # Create the sorted list of nodes and their marginal gain 
+    Q = sorted(zip(candidates,marg_gain), key = lambda x: x[1],reverse=True)
+
+    # Select the first node and remove from candidate list
+    selected, spread, Q = [Q[0][0]], Q[0][1], Q[1:]
+    # timelapse = [time.time() - start_time]
+    
+    # Find the next budget-1 nodes using the CELF list-sorting procedure
+    
+    for _ in range(budget-1):    
+
+        check = False      
+        while not check:
+            
+            # Recalculate spread of top node
+            current = Q[0][0]
+            
+            # Evaluate the spread function and store the marginal gain in the list
+            Q[0] = (current, s.mean(IC(g, config, selected+[current])) - spread)
+
+            # Re-sort the list
+            Q = sorted(Q, key = lambda x: x[1], reverse=True)
+
+            # Check if previous top node stayed on top after the sort
+            check = Q[0][0] == current
+
+        # Select the next node
+        selected.append(Q[0][0])
+        spread = Q[0][1]
+        # timelapse.append(time.time() - start_time)
+        
+        # Remove the selected node from the list
+        Q = Q[1:]
+        print(sorted(selected))
+    
+    return(sorted(selected))
+    # return(sorted(S),timelapse)
 
 # baseline 9.2 CELFPP
+# Function definition with inputs of a graph 'g', a configuration 'config', and a budget 'budget'
 def celfpp(g, config, budget):
+
+    # Initialize 'selected' as an empty list. This list will eventually hold the selected nodes
     selected = []
+    
+    # Get a list of all nodes in the graph 'g'
     candidates = list(g.nodes())
 
+    # Initialize 'gains' as an empty list. This list will hold the gain of each node
     gains = []
+
+    # For each node in the list of candidates
     for node in candidates:
+        # Create a new list 'seed' that includes all currently selected nodes plus the current node
         seed = selected + [node]
+        
+        # Run the IC function on the graph with the 'seed' list and get the result
         result = IC(g, config, seed)
+        
+        # Calculate the average (mean) of the result and assign it to 'gain'
         gain = s.mean(result)
+        
+        # Add a tuple of negative 'gain', 'node', and 'None' (which will later be replaced with the last seed added) to the 'gains' list
         heapq.heappush(gains, (-gain, node, None))
 
+    # Initialize 'last_seed' as 'None'. This variable will hold the last seed that was added to 'selected'
     last_seed = None
 
+    # Repeat the next steps 'budget' times
     for i in range(budget):
+        # Keep looping until a condition is met
         while True:
+            # Get the tuple with the highest gain (which will be the first in the list due to the sorting in the heap)
             gain, node, last_added_seed = heapq.heappop(gains)
 
+            # If 'last_added_seed' equals 'last_seed'
             if last_added_seed == last_seed:
+                # Then the gain doesn't need to be recomputed, so set 'new_gain' to the negation of 'gain'
                 new_gain = -gain
             else:
+                # Otherwise, create a new list 'seed' that includes all currently selected nodes plus the current node
                 seed = selected + [node]
+                
+                # Run the IC function on the graph with the 'seed' list and get the result
                 result = IC(g, config, seed)
+                
+                # Calculate the average (mean) of the result and assign it to 'new_gain'
                 new_gain = s.mean(result)
 
+            # If 'new_gain' equals the negation of 'gain'
+            if new_gain == -gain:
+                # Break the loop
+                break
+            else:
+                # Otherwise, add the tuple of negative 'new_gain', 'node', and 'last_seed' back to the 'gains' list
+                heapq.heappush(gains, (-new_gain, node, last_seed))
+
+        # Append the current node to the 'selected' list
+        selected.append(node)
+        
+        # Remove the current node from the 'candidates' list
+        candidates.remove(node)
+        
+        # Set 'last_seed' to the current node
+        last_seed = node
+
+    # Print the sorted list of selected nodes
+    print(sorted(selected))
+    
+    # Return the list of selected nodes
+    return selected
+
+def celfpp2(g, config, budget):   
+    """
+    Inputs: g:     Network graph
+            config: Configuration object for the IC model
+            budget:     Size of seed set
+    Return: A seed set of nodes as an approximate solution to the IM problem
+    """
+      
+    # Compute marginal gain for each node
+    candidates = list(g.nodes())
+    marg_gain = [s.mean(IC(g, config, [node])) for node in candidates]
+
+    # Create the sorted list of nodes and their marginal gain 
+    Q = sorted(zip(candidates, marg_gain), key = lambda x: x[1], reverse=True)
+
+    # Select the first node and remove from candidate list
+    selected, spread, Q = [Q[0][0]], Q[0][1], Q[1:]
+
+    # Initialize last_seed as the first selected node
+    last_seed = selected[0]
+    
+    # Find the next budget-1 nodes using the CELF++ procedure
+    for _ in range(budget - 1):    
+        check = False
+        while not check:
+            # Get current node and its previous computed marginal gain
+            current, old_gain = Q[0][0], Q[0][1]
+
+            # Check if the last added seed has changed
+            if current != last_seed:
+                # Compute new marginal gain
+                new_gain = s.mean(IC(g, config, selected+[current])) - spread
+            else:
+                # If the last added seed hasn't changed, the marginal gain remains the same
+                new_gain = old_gain
+
+            # Update the marginal gain of the current node
+            Q[0] = (current, new_gain)
+
+            # Re-sort the list
+            Q = sorted(Q, key = lambda x: x[1], reverse=True)
+
+            # Check if previous top node stayed on top after the sort
+            check = Q[0][0] == current
+
+        # Select the next node
+        selected.append(Q[0][0])
+        spread += Q[0][1]  # Update the spread
+        last_seed = Q[0][0]  # Update the last added seed
+
+        # Remove the selected node from the list
+        Q = Q[1:]
+
+        print(sorted(selected))
+
+    return sorted(selected)
+
+    """
+    Inputs: g:     Network graph
+            config: Configuration object for the IC model
+            budget:     Size of seed set
+    Return: A seed set of nodes as an approximate solution to the IM problem
+    """
+      
+    # Compute marginal gain for each node
+    candidates = list(g.nodes())
+
+    # Initialize the gains list as a min heap for efficiency
+    gains = []
+
+    # For each candidate node, compute its marginal gain and push it into the heap along with the node itself
+    for node in candidates:
+        gain = s.mean(IC(g, config, [node]))
+        heapq.heappush(gains, (-gain, node, None)) # Initial last added seed is None
+
+    # Track the last added seed node
+    last_seed = None
+
+    # Select the first node and remove from candidate list
+    selected = []
+
+    # Repeat the next steps 'budget' times
+    for _ in range(budget):
+        while True:
+            # Pop the node with highest gain from heap
+            gain, node, last_added_seed = heapq.heappop(gains)
+            
+            # Check if the gain needs to be updated
+            if last_added_seed == last_seed: # If the last added seed hasn't changed
+                new_gain = -gain # No need to recompute gain
+            else:
+                # Otherwise, recompute the gain
+                new_gain = s.mean(IC(g, config, selected+[node])) 
+
+            # If the recomputed gain remains the same, break the loop
             if new_gain == -gain:
                 break
             else:
-                heapq.heappush(gains, (-new_gain, node, last_seed))
+                # Otherwise, push the node with its new gain into the heap
+                heapq.heappush(gains, (-new_gain, node, last_seed)) # Update last added seed
 
+        # Append the current node to the 'selected' list
         selected.append(node)
+        # Remove the current node from the 'candidates' list
         candidates.remove(node)
+        # Update the last seed added
         last_seed = node
+        print(sorted(selected))
+    
+    return sorted(selected)
 
-    return selected
 
 # greedy -> CELF/CELFPP -> IC/LT
 def greedy(g, config, budget):
@@ -340,12 +580,14 @@ def greedy(g, config, budget):
 
         selected.append(index)
         candidates.remove(index)
+    print(sorted(selected))
 
     return selected
 
+# fixme set MC = 100
 def IC(g, config, seed):
     # number of Monte Carlo simulations to be run for the IC model
-    mc_number = 100
+    mc_number = 2
     result = []
 
     for iter in range(mc_number):
