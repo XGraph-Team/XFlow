c0f84f8e5d7344027a9b8c3e2a92373959e56a66
zz242msu <121355649+zz242msu@users.noreply.github.com>
2023-05-31 09:50:52 -0400

    Update SIM-LT.py

--- benchmark/SIM-LT.py
+++ benchmark/SIM-LT.py
@@ -2,7 +2,7 @@ import networkx as nx
 from time import time
 
 from graphGeneration import Cora, CiteSeer, PubMed, connSW, ER, coms, photo
-from IM import eigen, degree, Netshield, SoboldegLT, SoboleigenLT, SobolNSLT, greedyLT
+from baselines import eigen, degree, pi, sigma, greedy, celf, celfpp, IMRank, RIS
 from score import effectLT
 
 g, config = Cora()
