aca10713d4b6b3f4559e315c7181373f5336b948
zz242msu <zz242@msstate.edu>
2023-06-17 18:40:38 -0400

    sl runs v0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = 'https://xflow.network/'
 EMAIL = 'zchen@cse.msstate.edu'
 AUTHOR = 'XGraphing'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.10'
+VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
